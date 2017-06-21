d5823dcec1748382f2dfa6491e1ae6feb3f3a3b0
Benjamin Schubert <brm.schubert@gmail.com>
2017-06-21 17:46:02 -0400

    Documented ImageStacker

--- processing/imagestacker.h
+++ processing/imagestacker.h
@@ -18,7 +18,10 @@ class ImageStacker : public QObject
     friend class TestOSS;
 #endif
 public:
+    //! Constructor for ImageStacker.
     explicit ImageStacker(QObject *parent = 0);
+
+    //! Flag set to asynchronously cancel processing.
     bool cancel;
 
     //! The extensions that the app will treat as RAW images.
@@ -36,6 +39,10 @@ public:
     */
     ImageRecord* getImageRecord(QString filename);
 
+    //! Gets an OpenCV Mat from the image at the specified filename.
+    /*!
+        @param filename The image file to read.
+    */
     cv::Mat readImage(QString filename);
 
     // get/set
@@ -85,15 +92,26 @@ public:
     void setUseBias(bool value);
 
 signals:
-    //! Emitted when the processing is finished.
+    //! Provides the final image when the processing is finished.
     /*!
         @param image The final processed image.
     */
     void finished(cv::Mat image);
+
+    //! Provides a message when processing is finished, and marks the processing as complete.
+    /*!
+        @param message The message to accompany the process completion.
+    */
     void finishedDialog(QString message);
 
     //! Provides a percentage of completion and a description of what's happening.
+    /*!
+        @param message A description of the current section of processing.
+        @param percentComplete An integer, out of 100, representing the percentage of completion.
+    */
     void updateProgress(QString message, int percentComplete);
+
+    //! Emits a QImage after asynchronously reading it.
     void QImageReady(QImage image);
 
     //! Emitted when an error occurs during processing.
@@ -101,21 +119,18 @@ signals:
 public slots:
     //! The main method for processing the images.
     void process();
+
+    //! Gets an image from disk and asynchronously reads it as a QImage.
+    /*! Emits QImageReady once the image is ready.
+        @param filename The image file to read.
+    */
     void readQImage(QString filename);
 
 private:
-    //! Aligns the target image to the reference image and returns it.
-    /*!
-        @param ref The reference image.
-        @param target The image to be aligned.
-    */
+
     cv::Mat generateAlignedImage(cv::Mat ref, cv::Mat target);
     cv::Mat averageImages(cv::Mat img1, cv::Mat img2);
 
-    //! Checks that all checked images are the same size
-    /*!
-        @return 0 if all images are validated; -1 otherwise.
-    */
     int validateImageSizes();
 
     QImage Mat2QImage(const cv::Mat &src);
