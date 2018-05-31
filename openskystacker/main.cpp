5aacd5d3f3d7da637d58b0fbb411c7a0e7971a49
Benjamin Schubert <brm.schubert@gmail.com>
2018-05-31 15:04:26 -0400

    Use spdlog as QDebug message handler
    Rotate between two 1MB log files

--- openskystacker/main.cpp
+++ openskystacker/main.cpp
@@ -2,11 +2,65 @@
 #include <QApplication>
 #include <QTranslator>
 
+#include "spdlog/spdlog.h"
+
 // cv::Mat can't be passed through a signal without this declaration
 Q_DECLARE_METATYPE(cv::Mat)
 
+std::shared_ptr<spdlog::logger> logger;
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &qMsg) {
+    if (!logger) {
+        return;
+    }
+
+    const char *msg = qMsg.toUtf8().constData();
+
+    switch (type) {
+    case QtDebugMsg:
+        logger->debug(msg);
+        break;
+    case QtInfoMsg:
+        logger->info(msg);
+        break;
+    case QtWarningMsg:
+        logger->warn(msg);
+        break;
+    case QtCriticalMsg:
+        logger->critical(msg);
+        break;
+    case QtFatalMsg:
+        logger->critical("FATAL: %s", msg);
+        QApplication::exit(1);
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+
+#ifdef Q_OS_UNIX
+    // On UNIX platforms, put the logs in ~/.openskystacker
+    QString qLogDir = QDir::homePath() + "/.openskystacker";
+    QDir dir(qLogDir);
+    if (!dir.exists()) {
+        dir.mkpath(".");
+    }
+    std::string logDir = qLogDir.toUtf8().constData();
+#else
+    // on Windows, put the logs beside the application
+    std::string logDir = QApplication::applicationDirPath().toUtf8().constData();
+#endif
+
+#ifndef QT_NO_DEBUG
+    // Only show Debug-level messages when built in debug mode
+    spdlog::set_level(spdlog::level::debug);
+#endif
+
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
+    logger = spdlog::rotating_logger_mt("logger", logDir + "/log.txt", 1048576, 2);
+    qInstallMessageHandler(messageHandler);
+
     QApplication a(argc, argv);
 
 //    QTranslator translator;
